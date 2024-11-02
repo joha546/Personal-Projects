70b8119fc1f0e541c2dd48c62abaa25bbb0371fa
Md Khaled Bin Joha <joh60483@gmail.com>
2024-11-03 01:32:38 +0600

    Controllers & Views test

---
+++ ASP-NET/MVC-Pattern/MVC-Pattern/Controllers/ClubController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Pattern.Controllers
+{
+    public class ClubController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP-NET/MVC-Pattern/MVC-Pattern/Controllers/RaceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Pattern.Controllers
+{
+    public class RaceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
