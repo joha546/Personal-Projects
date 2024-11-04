ae0291082e2fb8967b61551b5f4f9bed6a06cd75
Md Khaled Bin Joha <joh60483@gmail.com>
2024-11-05 00:37:38 +0600

    Completed view route with seed data

--- ASP-NET/MVC-Pattern/MVC-Pattern/Controllers/ClubController.cs
+++ ASP-NET/MVC-Pattern/MVC-Pattern/Controllers/ClubController.cs
@@ -1,13 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Pattern.Data;
+using MVC_Pattern.Models;
 
 namespace MVC_Pattern.Controllers
 {
     public class ClubController : Controller
     {
-
+        private readonly ApplicationDbContext _context;
+        public ClubController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Club> clubs = _context.Clubs.ToList();
+            return View(clubs);
         }
     }
 }
--- ASP-NET/MVC-Pattern/MVC-Pattern/Controllers/RaceController.cs
+++ ASP-NET/MVC-Pattern/MVC-Pattern/Controllers/RaceController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using MVC_Pattern.Data;
+using MVC_Pattern.Models;
 
 namespace MVC_Pattern.Controllers
 {
     public class RaceController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public RaceController(ApplicationDbContext context) { 
+            this._context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Race> races = _context.Races.ToList();
+            return View(races);
         }
     }
 }
